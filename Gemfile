aab5f1e9661ea4bc388ffd7ceaa05e30b5c7b3b3
Waclaw Gajda <waclaw.gajda@gmail.com>
2018-01-12 14:57:49 +0100

    model+widok car

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ end
 
 gem 'carrierwave'
 gem 'pry-rails'
+gem 'bootstrap-sass', '~> 3.3.7'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,34 @@
 class CarsController < ApplicationController
+  before_action :set_car, except: [:index, :new, :create]
   def index
+    @car = Car.all.order(mark: :desc)
+    @car = @car.where("mark like ?", params[:q]) if params[:q].present?
+  end
+  def new
+    @car = Car.new
+  end
+  def create
+    @car = Car.create(car_params)
+    render 'cars/show'
+  end
+  def edit
+  end
+  def update
+    @car.update_attributes(car_params)
+    render 'cars/edit'
+  end
+  def show
+  end
+  def destroy
+    @car.destroy
+    redirect_to action: "index"
   end
 
   private
+  def set_car
+    @car = Car.find(params[:id])
+  end
   def car_params
-    params.require(:car).permit(:mark, :model, :image)
+    params.require(:car).permit(:mark, :model, :image, :year_prod, :prince, :description, :mileage, :fuel)
   end
 end
