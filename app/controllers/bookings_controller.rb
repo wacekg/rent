245d513b5a54cfc43c56f1bbef6d631b74bc12e5
Waclaw Gajda <waclaw.gajda@gmail.com>
2018-01-28 16:50:56 +0100

    booking _form nie sprawdzone

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,24 @@ class BookingsController < ApplicationController
   def index
     @bookings = Booking.all.order(mark: :desc)
   end
+  def new
+    @booking = Booking.new
+  end
+  def create
+    @booking = Booking.create(booking_params)
+    render 'bookings/show'
+  end
+  def new
+  end
+  def edit
+  end
+  def show
+  end
+  def destory
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:last_name, :first_name, :email, :phone, :started_at,:ended_at, car_id)
+  end
 end
