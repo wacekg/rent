08b36e316c6a9bdf8c855d07d11c56accb3d8cc2
Waclaw Gajda <waclaw.gajda@gmail.com>
2018-02-08 15:13:50 +0100

    booking - samochod dostępny

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
   before_action :set_booking, except: [:index, :new, :create]
+  before_action :car_params, only: [:create, :update]
   def index
     @bookings = Booking.all.order(started_at: :desc)
   end
@@ -7,16 +8,17 @@ class BookingsController < ApplicationController
     @booking = Booking.new
   end
   def create
-    @booking = Booking.create(booking_params)
+    if @car.access
+      @booking = Booking.create(booking_params)
+    end
     render 'bookings/show'
   end
   def edit
   end
   def update
-    # if @booking.car.access
-    # binding.pry
+    if @car.access
       @booking.update_attributes(booking_params)
-    # end
+    end
     render 'bookings/edit'
   end
   def show
@@ -28,9 +30,12 @@ class BookingsController < ApplicationController
 
   private
   def booking_params
-    params.require(:booking).permit(:last_name, :first_name, :email, :address, :phone, :started_at,:ended_at, :car_id, :approved)
+    params.require(:booking).permit(:last_name, :first_name, :email, :address, :phone, :started_at, :ended_at, :car_id, :approved)
   end
   def set_booking
     @booking = Booking.find(params[:id])
+  end
+  def car_params
+    @car=Car.find(booking_params[:car_id])
   end
 end
